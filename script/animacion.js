fdae9d97a4de5a81210d3755fd9bc48331970e34
Ricardo Polanco <polancoblancoricardo@gmail.com>
2023-07-06 20:06:22 -0600

    slider effect

--- script/animacion.js
+++ script/animacion.js
@@ -13,3 +13,45 @@ if(cerrar){
         nav.classList.remove('active');
     });
 ;}
+
+// publicidad -----------------------------------------
+const slider = document.querySelector('#slider'),
+btnLeft = document.querySelector('#btn-left'),
+btnRight =document.querySelector('#btn-right');
+let sliderSection = document.querySelectorAll('.sliderSection');
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function moverDerecha(){
+    let sliderSectionFirst = document.querySelectorAll('.sliderSection')[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend',sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    },500);
+};
+
+function moverIzquierda(){
+    let sliderSection = document.querySelectorAll('.sliderSection');
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    },500);
+};
+
+btnRight.addEventListener('click',function(){
+    moverDerecha();
+})
+btnLeft.addEventListener('click',function(){
+    moverIzquierda();
+});
+setInterval(function(){
+    moverDerecha();
+},3000);
